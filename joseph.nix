a293c178dcedb01b51b26fb928ff9e96f990f322
bretek <josephmadden999@protonmail.com>
2025-01-01 20:35:32 +0000

    task: added program steam

--- joseph.nix
+++ joseph.nix
@@ -23,6 +23,12 @@
     firefox
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   programs.zsh.enable = true;
 }
 
