155b027ddd1e43cdae73469fdf4f4a8645dfaaae
bretek <josephmadden999@gmail.com>
2025-01-19 13:13:15 +0000

    feat: added laptop power management services

--- nixos/default.nix
+++ nixos/default.nix
@@ -43,6 +43,7 @@
     };
   };
 
+  powerManagement.enable = true;
   hardware.bluetooth.enable = true;
   security.polkit.enable = true;
   services = {
@@ -50,6 +51,28 @@
     pipewire.enable = false;
     printing.enable = true;
     blueman.enable = true;
+    thermald.enable = true;
+
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+
+        #Optional helps save long term battery health
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+
+      };
+    };
 
     greetd = {
       enable = true;
